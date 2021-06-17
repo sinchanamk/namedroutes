1cd5fa8ff18c176752a7f6e6a9bbe90d0131edd5
sinchana M K <mksinchanasonu@gmail.com>
2021-06-17 22:38:17 +0530

    first

---
+++ lib/first.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/second.dart';
+class Firstscreen extends StatelessWidget {
+  const Firstscreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('FirstScreen'),),
+      body: SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            Image.asset('assets/q.jpg',height: MediaQuery.of(context).size.height,
+               ),
+               Container(
+          width: 145.0,
+          height:49.0,padding:EdgeInsets.only(right: 10.0,left: 7.0,top:20.0) ,
+          child:  FlatButton(color: Colors.pink,
+    
+              onPressed: () => { Navigator.pushNamed(context, "/second"),
+              }, child: Text('View All',style: TextStyle(color: Colors.black),),
+              ),
+          ), 
+          ],
+        ),
+      ),
+      
+      
+      ),
+    );
+  }
+}
